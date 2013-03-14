df6be49052598986550ce2bb5273ee277282adcf
root <root@enrique-HP-ProBook-4430s.(none)>
2013-03-14 17:00:01 -0500

    Thu Mar 14 17:00:01 COT 2013

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
 <?php
 
 // change the following paths if necessary
-$yii=dirname(__FILE__).'/../yii/framework/yiilite.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
 
+$yii=dirname(__FILE__).'/../yii/framework/yiilite.php';
+//echo $yii;
+$config=dirname(__FILE__).'/protected/config/main.php';
+//echo $config;
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
