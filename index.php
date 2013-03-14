3c3fb5e1146f09f119504610a41bab7161df6b16
root <root@enrique-HP-ProBook-4430s.(none)>
2013-03-14 17:05:01 -0500

    Thu Mar 14 17:05:01 COT 2013

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 // change the following paths if necessary
 
 $yii=dirname(__FILE__).'/../yii/framework/yiilite.php';
-//echo $yii;
+
 $config=dirname(__FILE__).'/protected/config/main.php';
 //echo $config;
 // remove the following lines when in production mode
@@ -11,5 +11,7 @@ defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
 require_once('../FirePHPCore/FirePHP.class.php');
+
 require_once($yii);
+
 Yii::createWebApplication($config)->run();
